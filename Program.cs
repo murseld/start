e993a8a00b4ec191eba29df586961f045df7b839
Mursel Dogrusozlu <mudog@dfds.com>
2023-06-07 14:58:04 +0300

    folder structure3

--- Program.cs
+++ Program.cs
@@ -4,55 +4,69 @@ namespace HelloWorld
 {
     class Program
     {
-
         static List<string> packageList = new List<string>();
+
         static List<string> documentTypeList = new List<string>();
+
         static void Main(string[] args)
         {
-
-            var path = @"C:\workspaces\integration\ci_esb_backend\assets\IS\Packages\ICCPhoenix";
+            Console.WriteLine("Enter Enviroment Name (BE or FE)");
+            string? envName = Console.ReadLine();
+            envName = string.IsNullOrEmpty(envName) ? "ICCPhoenix" : envName;
+            envName =
+                envName == "BE" ? "ci_esb_is_backend" : "ci_esb_is_frontend";
+            Console.WriteLine("Enter Package Name");
+            string? packageName = Console.ReadLine();
+            packageName =
+                string.IsNullOrEmpty(packageName) ? "ICCPhoenix" : packageName;
+            var path =
+                @"C:\workspaces\integration\" +
+                envName +
+                @"\assets\IS\Packages\" +
+                packageName;
             if (File.Exists(path))
             {
                 // This path is a file
-                ProcessFile(path);
+                ProcessFile (path);
+                Log();
             }
             else if (Directory.Exists(path))
             {
                 // This path is a directory
-                ProcessDirectory(path);
+                ProcessDirectory (path);
+                Log();
             }
             else
             {
-                Console.WriteLine("{0} is not a valid file or directory.", path);
+                Console
+                    .WriteLine("{0} is not a valid file or directory.", path);
             }
-            Log();
         }
 
-        // Process all files in the directory passed in, recurse on any directories 
+        // Process all files in the directory passed in, recurse on any directories
         // that are found, and process the files they contain.
-        static string packageName = string.Empty;
         public static void ProcessDirectory(string targetDirectory)
         {
-
             // Process the list of files found in the directory.
             string[] fileEntries = Directory.GetFiles(targetDirectory);
+            string packageName = string.Empty;
+
             foreach (string fileName in fileEntries)
             {
-                ProcessFile(fileName);
+                ProcessFile (fileName);
             }
 
             // Recurse into subdirectories of this directory.
-            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            string[] subdirectoryEntries =
+                Directory.GetDirectories(targetDirectory);
             foreach (string subdirectory in subdirectoryEntries)
             {
                 var dirName = subdirectory.Split('\\');
                 if (!packageName.Equals(dirName))
                 {
-                    //Console.WriteLine(subdirectory.Split('\\')[7]);
                     packageName = dirName[7];
                 }
-                ProcessDirectory(subdirectory);
-                //Console.WriteLine(subdirectory);
+                ProcessDirectory (subdirectory);
             }
         }
 
@@ -61,50 +75,62 @@ namespace HelloWorld
         {
             if (Path.GetFileName(path) == "flow.xml")
             {
-
-                GetFlowDependency(path);
+                GetFlowDependency (path);
             }
             else if (Path.GetFileName(path) == "node.ndf")
             {
-
-                GetDocumentTypeDependency(path);
+                GetDocumentTypeDependency (path);
             }
         }
 
-
         public static void GetFlowDependency(string path)
         {
-            string[] attributes = { "INVOKE", "MAPINVOKE" };
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(path);
-            foreach (var attribute in attributes)
+            try
             {
-                XmlNodeList n = xmlDoc.GetElementsByTagName(attribute);
-                if (n != null)
+                string[] attributes = { "INVOKE", "MAPINVOKE" };
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load (path);
+                foreach (var attribute in attributes)
                 {
-                    foreach (XmlNode curr in n)
+                    XmlNodeList n = xmlDoc.GetElementsByTagName(attribute);
+                    if (n != null)
                     {
-                        var packageName = curr?.Attributes?["SERVICE"]?.Value;
-                        if (!packageList.Any(p => p == packageName) && packageName != null)
+                        foreach (XmlNode curr in n)
                         {
-
-                            packageList.Add(packageName);
+                            var packageName =
+                                curr?.Attributes?["SERVICE"]?.Value;
+                            if (
+                                !packageList.Any(p => p == packageName) &&
+                                packageName != null
+                            )
+                            {
+                                packageList.Add (packageName);
+                            }
                         }
-                        //Console.WriteLine("--" + curr.Attributes["SERVICE"].Value);
                     }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " " + path);
             }
         }
 
         public static void GetDocumentTypeDependency(string path)
         {
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(path);
+            xmlDoc.Load (path);
             XmlNodeList n = xmlDoc.GetElementsByTagName("rec_ref");
             if (xmlDoc.DocumentElement != null)
             {
-                XmlNode? node = xmlDoc.DocumentElement.SelectSingleNode("//value[@name='rec_ref']");
-                if (node != null && !documentTypeList.Any(p => p == node.InnerText))
+                XmlNode? node =
+                    xmlDoc
+                        .DocumentElement
+                        .SelectSingleNode("//value[@name='rec_ref']");
+                if (
+                    node != null &&
+                    !documentTypeList.Any(p => p == node.InnerText)
+                )
                 {
                     documentTypeList.Add(node.InnerText);
                 }
@@ -114,15 +140,15 @@ namespace HelloWorld
         private static void Log()
         {
             Console.WriteLine("-------------Packages List-------------");
-            foreach (var item in packageList)
+            foreach (var item in packageList.OrderBy(x => x).ToArray())
             {
-                Console.WriteLine(item);
+                Console.WriteLine (item);
             }
             Console.WriteLine("--------------------------------------");
             Console.WriteLine("----------DocumentTypes List----------");
-            foreach (var item in documentTypeList)
+            foreach (var item in documentTypeList.OrderBy(x => x).ToArray())
             {
-                Console.WriteLine(item);
+                Console.WriteLine (item);
             }
             Console.WriteLine("--------------------------------------");
         }
